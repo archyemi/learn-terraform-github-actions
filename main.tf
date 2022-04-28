5da38ad41a6b772257ff388125c37cc37583572e
Adeyemi Alatishe <36404707+archyemi@users.noreply.github.com>
2022-04-28 16:07:14 +0100

    new

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = "ami-04505e74c0741db8d"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
@@ -57,7 +57,7 @@ resource "aws_instance" "web" {
               EOF
 }
 
-resource "aws_security_group" "web-sg" {
+resource "aws_security_group" "web-sg-2" {
   name = "${random_pet.sg.id}-sg"
   ingress {
     from_port   = 8080
