9180e64640cf93cc72c0a3c6f9754742983843f8
Adeyemi Alatishe <36404707+archyemi@users.noreply.github.com>
2022-05-03 19:52:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
-  secret_key = ${{ sercrets.AWS_SECRET_ACCESS_KEY }}
+  #access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  #secret_key = ${{ sercrets.AWS_SECRET_ACCESS_KEY }}
 }
 
 resource "random_pet" "sg" {}
