13970fc2c793f2c15fe9a9c25bf39db51adac5b1
Adeyemi Alatishe <36404707+archyemi@users.noreply.github.com>
2022-05-03 19:55:37 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,11 +20,11 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
+#provider "aws" {
+  #region = "us-east-1"
   #access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
   #secret_key = ${{ sercrets.AWS_SECRET_ACCESS_KEY }}
-}
+#}
 
 resource "random_pet" "sg" {}
 
